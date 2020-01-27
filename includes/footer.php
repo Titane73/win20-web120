3262c1ea72785b7c9fdd7471da53aeec93b4afe1
Ti Hall <55930349+Titane73@users.noreply.github.com>
2020-01-26 16:29:40 -0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<!-- Start footer.php-->
+       
+<!-- Start footer     -->
+     <footer>
+      <p><small>&copy; 2020 by <a href="contact.php">Ti Hall </a>, All Rights Reserved ~ <a href="https://validator.w3.org/check/referer" target="_blank">HTML5</a> ~ <a href="https://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">CSS3</a></small></p>
+    </footer>
+  </main>
+     
+  <!-- Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon -->
+     
+  <script>
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }   
+  </script>
+     
+ </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Ti Hall: WEB120 Portal Website</title>
+    <meta charset="utf-8" />
+    <meta name="robots" content="noindex,nofollow" />
+    <meta name="viewport" content="width=device-width" />
+    <link rel="stylesheet" href="css/portal.css" />
+    <link rel="stylesheet" href="css/nav.css" />
+    <link rel="stylesheet" href="css/forms.css" />
+</head>
+
+<body>
+    <header>
+        <h1><a href="index.php">Ti Hall: WEB120 Portal Website</a></h1>
+        <nav class="topnav" id="myTopnav">
+            <a href="index.php" class="active">Welcome</a>
+            <!--<a href="elements.php">HTML Elements</a>-->
+            <a href="aia.php">AIA</a>
+            <a href="flowchart.php">Flowchart</a>
+            <a href="big/index.php">BIG</a>
+            <!--<a href="http://w3schools.com" target="_blank">W3 Schools</a>-->
+            <!--<a href="https://www.linkedin.com/learning/" target="_blank">LinkedIn Learning</a>-->
+            <a href="fp/index.html">Final Project</a>
+            <a href="contact.php">Contact Ti</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+        </nav>
+    </header>
+    <!-- start header.php   -->
